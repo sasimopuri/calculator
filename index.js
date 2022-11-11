5deadf4e52da8ec14d397afef55b138603371b5e
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2022-11-11 23:54:11 +0530

    Half done

---
+++ index.js
@@ -0,0 +1,27 @@
+let exp='';
+let output='';
+let dexp=document.querySelector(".exp");
+let dans=document.querySelector(".ans")
+function btn(val){
+    console.log(val.innerHTML);
+    
+         exp+=val.innerHTML
+        dexp.innerHTML=exp
+    
+}
+
+function evaluating(){
+    let ans=eval(exp);
+    dans.innerHTML=ans;
+    
+}
+
+function clearr(){
+    exp='';
+    ans=''
+    exp.innerHTML=''
+    dexp.innerHTML=''
+    dans.innerHTML=''
+    console.log('da');
+}
+
